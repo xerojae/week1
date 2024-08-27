87c1b561c1c0016b119141525a8167b60fcdded9
Jaeden Warner <xerojae@Jaedens-MacBook-Pro.local>
2024-08-27 11:32:13 -0400

    initial commit

---
+++ week 1/DemoHeap.java
@@ -0,0 +1,6 @@
+class DemoHeap{
+
+    public int add(int x, int y){
+        return x + y;
+    }
+}
---
+++ week 1/demo.java
@@ -0,0 +1,16 @@
+public class demo{
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = 20;
+
+        DemoHeap ob1 = new DemoHeap();
+        int c = ob1.add(a,b);
+
+        System.out.println(c);
+
+    }
+
+
+
+}
